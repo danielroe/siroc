97dec0a739199aea33264feccb3acd5dc0c3a80c
Daniel Roe <daniel@concision.co.uk>
2020-06-14 11:17:50 +0100

    chore: allow class method overloads

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 0,
     '@typescript-eslint/explicit-module-boundary-types': 0,
     'jest/valid-describe': 0,
+    'no-dupe-class-members': 0,
   },
   extends: [
     'plugin:promise/recommended',
