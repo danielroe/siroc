60c99203e812b6bc0df3b270e3d21dc9acc36404
Daniel Roe <daniel@roe.dev>
2021-01-16 23:40:21 +0000

    fix: support matching entrypoints in subdirectories

--- src/core/package/utils.ts
+++ src/core/package/utils.ts
@@ -1,27 +1,36 @@
-import { basename } from 'path'
+import { basename, sep } from 'path'
 import type { PackageJson } from './types'
+
+const walkDownDirectory = (pathname: string) => {
+  const [first, ...rest] = pathname.split(sep)
+  return rest.join(sep)
+}
 
 export const getEntrypointFilenames = (path: string) => {
   if (path.startsWith('./')) path = path.slice(2)
 
-  const basefile = basename(path).split('.').slice(0, -1).join('.')
-  const withoutType = basefile.split('.').slice(0, 1).join('')
-  const filenames = Array.from(
-    new Set([
+  const filenames = new Set<string>()
+  let cwd = path
+  do {
+    const basefile = cwd.split('.').slice(0, -1).join('.')
+    const withoutType = basefile.split('.').slice(0, 1).join('')
+    ;[
       basefile,
       `${basefile}/index`,
       withoutType,
       `${withoutType}/index`,
-      'index',
-    ])
-  )
+    ].forEach(name => filenames.add(name))
+    cwd = walkDownDirectory(cwd)
+  } while (cwd)
+
+  filenames.add('index')
+
+  return Array.from(filenames)
     .map(name => [`${name}.ts`, `${name}.js`])
     .reduce((names, arr) => {
       arr.forEach(name => names.push(name))
       return names
     }, [] as string[])
-
-  return filenames
 }
 
 export const getFlatValues = (
